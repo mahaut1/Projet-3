a73579e3e506aee0e7c12f9614bf9fbc2c681184
mahaut1 <mwindal@hotmail.com>
2023-08-21 20:30:24 +0200

    rattrapage de conneries + dossier js

---
+++ FrontEnd/js/script.js
@@ -0,0 +1,28 @@
+	// Connexion à l'api en async pour que le programme continue à fonctionner pendant le chargement. Une fois que l'image est chargée , la fonction async renvoie les données que je peux utiliser pour afficher l'image correctement sur le site
+    const getApi = async()=>{
+        try{
+            const response= await fetch("http://localhost:5678/api/works");
+            const data= await response.json();
+            return data;
+        }catch (error){
+            console.log(error);
+        }
+    };
+    // fonction pour créer les éléments et les récupérer
+    const dynamicGallery = (cards) => {
+        const gallery = document.querySelector(".gallery");
+        gallery.innerHTML="";
+        cards.forEach((card)=> {
+            const galleryCard=document.createElement("galleryCard");
+            gallery.appendChild(galleryCard);
+            galleryCard.innerHTML = `
+            <img src="${card.imageUrl}" alt="${card.title}">
+            <h3 class="cardTitle">${card.title}</h3>
+            `;
+        });
+    };
+
+    //affichage des données sur le site dynamiquement
+    getApi().then((data)=>{
+        dynamicGallery(data);
+    });
