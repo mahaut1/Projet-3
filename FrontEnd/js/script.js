1ed01084b87a442b44c8fd4cf2d43780a85c33ec
mahaut1 <mwindal@hotmail.com>
2023-08-23 16:29:54 +0200

    authentification login

--- FrontEnd/js/script.js
+++ FrontEnd/js/script.js
@@ -1,6 +1,4 @@
-	/**
-     * Affichage dynamique des catégories
-     */
+	/****** AFFICHAGE DES CATEGORIES************/
     
     const getCatApi = async()=>{
         try{
@@ -35,9 +33,14 @@
     buttonBar.appendChild(btnAll);
     btnAll.classList.add("FilterButton");
 
-    /*****
-     * Affichage dynamique des works 
-     ******/
+    btnAll.addEventListener("click",async()=>{
+        getWorkApi().then((data)=>{
+            dynamicGallery(data);
+        })
+    })
+
+    /********** AFFICHAGE DES WORKS ***************/
+   
     // Connexion à l'api en async pour que le programme continue à fonctionner pendant le chargement. Une fois que l'image est chargée , la fonction async renvoie les données que je peux utiliser pour afficher l'image correctement sur le site
     const getWorkApi = async()=>{
         try{
@@ -48,7 +51,7 @@
             console.log(error);
         }
     };
-    // fonction pour créer les éléments et les récupérer
+    // fonction pour créer les cards des works
     const dynamicGallery = (cards) => {
         const gallery = document.querySelector(".gallery");
         gallery.innerHTML="";
@@ -62,7 +65,55 @@
         });
     };
 
-    //affichage des données sur le site 
+    //affichage des cards des works
     getWorkApi().then((data)=>{
         dynamicGallery(data);
     });
+
+    /********************* LOGIN *****************/
+
+    function clearSessionStorage(){
+        sessionStorage.clear();
+        document.location.href ="login.html";
+    }
+
+    sessionStorage.clear();
+
+    //récupération du formulaire dans le DOM
+    const formulaire= document.getElementById("loginForm");
+
+    // écoute du bouton submit
+    formulaire.addEventListener("submit", event=>{
+        //empecher le rechargement de la page
+        event.preventDefault();
+
+        //récupération de la valeur des input
+        const email=document.getElementById("email").value;
+        const password=document.getElementById("password").value;
+
+        // connexion à la base de données
+        fetch('http://localhost:5678/api/users/login', {
+            methode:"POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({email, password})
+        })
+        .then(response => response.json())
+        .then(data =>{
+            let userId= data.useId;
+            if (userId == 1){
+                let token= data;
+                sessionStorage.setItem("token", token.token);
+
+                document.location.href="index.html";
+            } else {
+                let errorMessage= document.getElementById("error-message");
+                errorMessage.textContent="Identifiant ou mot de passe incorrect"
+            }
+        })
+        .catch(error =>{
+            console.error(error)
+        })
+
+    })
